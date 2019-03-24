88751151639a42eca3d89bbd043feb0a0fbf3e47
riskers <gaoyibobobo@gmail.com>
2019-03-25 03:33:18 +0800

    upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { KeyboardEvent } from 'react';
 import ReactDOM from 'react-dom';
+import domtoimage from 'dom-to-image';
 import './index.css';
 
-class App extends React.Component {
+interface IState {
+  name: string
+  email: string
+  address: string
+  phone: string
+  [field: string]: string
+}
+
+interface IProps {
+
+}
+
+class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+
+    this.state = {
+      name: '',
+      email: '',
+      address: '',
+      phone: ''
+    }
+  }
+
+  onChange = (field: string, value: string) => {
+    this.setState({
+      [field]: value,
+    })
+  }
+
   render() {
+    const {
+      name,
+      email,
+      address,
+      phone,
+    } = this.state
+
     return (
       <div className="App">
-        init
+        <div className="show">
+          {name}
+          {email}
+          {address}
+          {phone}
+        </div>
+
+        <div className="field">
+          <input value={name} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+
+            console.log(e.target.value)
+            this.onChange('name', e.target.value)
+          }} />
+          
+        </div>
       </div>
     );
   }
